685a81dbe6d4aac5fa836f0c322b686d3ef64d2c
Алина Красильникова <KrasilnikovaAA21@st.ithub.ru>
2023-11-05 11:54:29 +0300

    Added death mobs mechanic

--- Assets/Scripts/Enemies/MobController.cs
+++ Assets/Scripts/Enemies/MobController.cs
@@ -2,5 +2,40 @@ using UnityEngine;
 
 public class MobController : MonoBehaviour
 {
+    [SerializeField] private int maxHealth = 3; // Максимальное количество попаданий, чтобы уничтожить моб
+    [SerializeField] private int scoreValue = 10; // Количество очков за уничтожение моба
+    [SerializeField] private ScoreManager scoreManager;
+    private int currentHealth; // Текущее здоровье моба
 
+    private void Start()
+    {
+        currentHealth = maxHealth; // Инициализируем здоровье моба
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            TakeDamage();
+            Destroy(collision.gameObject); // Уничтожаем пулю после попадания
+        }
+    }
+
+    // Метод для обработки попадания пули
+    private void TakeDamage()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Метод вызывается при уничтожении моба
+    private void Die()
+    {
+        scoreManager.AddScore(scoreValue); // Увеличиваем счет
+        Destroy(gameObject); // Уничтожаем моб
+    }
 }
